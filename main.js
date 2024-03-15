16b994939e9aa47d3ba62241944ccb25c270beed
Nilanka Jayalath <160271650+Nilanka123@users.noreply.github.com>
2024-03-15 15:25:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Web Designer","Web Developer"],
+    typeSpeed:  100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+const contactForm = document.getElementById('')
